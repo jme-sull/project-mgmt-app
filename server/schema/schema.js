fe590b4303855fbe33352f5f84cb2ef00c7b595c
Jamie Sullivan <jkcrawshaw@gmail.com>
2023-03-17 07:00:21 -0400

    project crud

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -101,10 +101,41 @@ const mutation = new GraphQLObjectType({
 		deleteProject: {
 			type: ProjectType,
 			args: {
-				id: { type: GraphQLNonNull(GraphQLString) },
+				id: { type: GraphQLNonNull(GraphQLID) },
 			},
 			resolve(parent, args) {
-				Project.findByIdAndRemove(args.id);
+				return Project.findByIdAndRemove(args.id);
+			},
+		},
+		updateProject: {
+			type: ProjectType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLID) },
+				name: { type: GraphQLString },
+				description: { type: GraphQLString },
+				status: {
+					type: new GraphQLEnumType({
+						name: "ProjectStatusUpdate",
+						values: {
+							new: { value: "Not Started" },
+							progress: { value: "In Progress" },
+							completed: { value: "Completed" },
+						},
+					}),
+				},
+			},
+			resolve(parent, args) {
+				return Project.findByIdAndUpdate(
+					args.id,
+					{
+						$set: {
+							name: args.name,
+							description: args.description,
+							status: args.status,
+						},
+					},
+					{ new: true }
+				);
 			},
 		},
 	},
